e6a3149de875eab97d80f1d7925239f4b244e0a2
Sho SHIMIZU <sho.shimizu@gmail.com>
2021-07-31 15:07:37 +0900

    Inject stdin, stdout, stderr explicitly

--- cmd/lightnet/cmd/root.go
+++ cmd/lightnet/cmd/root.go
@@ -8,16 +8,24 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = cobra.Command{
-	Use:   "lightnet",
-	Short: "an CLI tool for lightnet",
-	Long:  "an CLI tool providing interactive ways to operate lightnet",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Run lightnet")
-	},
+func newRootCmd(in io.Reader, out, errOut io.Writer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "lightnet",
+		Short: "an CLI tool for lightnet",
+		Long:  "an CLI tool providing interactive ways to operate lightnet",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Run lightnet")
+		},
+	}
+	cmd.SetIn(in)
+	cmd.SetOut(out)
+	cmd.SetErr(errOut)
+
+	return cmd
 }
 
 func Execute(in io.Reader, out, errOut io.Writer) {
+	rootCmd := newRootCmd(in, out, errOut)
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
